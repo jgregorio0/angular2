ead65d4f177412f907e44370369658aaaff0bf93
Jesús Gregorio Pérez <gregoriojesus0@gmail.com>
2016-11-21 00:26:40 +0100

    Bloque II 9- Tour of Heroes : Routing

--- angular-tour-of-heroes/app/app.module.ts
+++ angular-tour-of-heroes/app/app.module.ts
@@ -3,31 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
 import { RouterModule }  from '@angular/router';
 
-import { HeroesComponent }      from './heroes.component';
-import { HeroDetailComponent }  from './hero-detail.component';
+import {HeroesComponent}      from './heroes.component';
+import {HeroDetailComponent}  from './hero-detail.component';
 import {HeroService}            from "./hero.service";
 import {AppComponent}           from "./app.component";
 import {DashboardComponent}     from "./dashboard.component";
+import {AppRoutingModule}       from "./app-routing.module";
 
 @NgModule({
     imports: [
         BrowserModule,
         FormsModule,
-        RouterModule.forRoot([
-            {
-                path: 'heroes',
-                component: HeroesComponent
-            },
-            {
-                path: 'dashboard',
-                component: DashboardComponent
-            },
-            {
-                path: '',
-                redirectTo: '/dashboard',
-                pathMatch: 'full'
-            }
-        ])],
+        AppRoutingModule],
     declarations: [
         AppComponent,
         HeroesComponent,
